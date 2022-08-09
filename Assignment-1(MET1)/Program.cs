c49bca7aecbddee9e5b16eaf8ef66eeb21036401
Jitin20 <jitinsharma2001@rediffmail.com>
2022-08-09 18:27:32 +0530

    commit

--- Assignment-1(MET1)/Program.cs
+++ Assignment-1(MET1)/Program.cs
@@ -40,20 +40,38 @@ namespace Assignment_1_MET1_
         {
             int runs = 0;
             int sum = 0;
+            Hashtable map = new Hashtable();
             Random rand = new Random();
             for(int i =0; i<=30; i++)
             {
                 runs = Convert.ToInt32(rand.Next(1, 6));
                 Console.WriteLine(runs);
                 sum = sum + runs;
+                if(map.ContainsKey(runs))
+                {
+                    map[runs] = Convert.ToInt32(map[runs]) + 1;
+                }
+                else
+                {
+                    map.Add(runs, 1);
+                }
             }
+            
             Console.WriteLine(sum);
+            foreach(int key in map.Keys)
+            {
+                String count = String.Format("Runs:{0} Count:{1}", key, map[key]);
+                Console.WriteLine(count);
+            }
+          
+            String strike = String.Format("Strike Rate : {0}", (sum / 30) * 100);
+            Console.WriteLine(strike);
+
            
         }
         static void Main(string[] args)
         {
             ElectricityBill();
-
             Batsman();
         }
 
---
+++ Assignment-1(MET1)/ProgramHelpers.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+namespace Assignment_1_MET1_
+{
+    internal static class ProgramHelpers
+    {
+
+        private static void Batsman()
+        {
+            int runs = 0;
+            int sum = 0;
+            Hashtable map = new Hashtable();
+            Random rand = new Random();
+            for (int i = 0; i <= 30; i++)
+            {
+                runs = Convert.ToInt32(rand.Next(1, 6));
+                Console.WriteLine(runs);
+                sum = sum + runs;
+                if (map.ContainsKey(runs))
+                {
+                    map[runs] = Convert.ToInt32(map[runs]) + 1;
+                }
+                else
+                {
+                    map.Add(runs, 1);
+                }
+            }
+
+            Console.WriteLine(sum);
+            foreach (int key in map.Keys)
+            {
+                String count = String.Format("Runs:{0} Count:{1}", key, map[key]);
+                Console.WriteLine(count);
+            }
+
+            String strike = String.Format("Strike Rate : {0}", (sum / 30) * 100);
+            Console.WriteLine(strike);
+
+
+        }
+    }
+}
